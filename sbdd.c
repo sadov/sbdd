e12bf46282daf3d055af221bbd53b91bf2c3aaf2
via8 <viaviii.gp@gmail.com>
2023-08-15 09:46:16 +0300

    impl corruption testbed

--- sbdd.c
+++ sbdd.c
@@ -10,10 +10,12 @@
 #include <linux/slab.h>
 #include <linux/numa.h>
 #include <linux/errno.h>
+#include <linux/delay.h>
 #include <linux/types.h>
 #include <linux/genhd.h>
 #include <linux/blkdev.h>
 #include <linux/string.h>
+#include <linux/random.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/vmalloc.h>
@@ -23,32 +25,42 @@
 #include <linux/blk-mq.h>
 #endif
 
-#define SBDD_SECTOR_SHIFT      9
-#define SBDD_SECTOR_SIZE       (1 << SBDD_SECTOR_SHIFT)
-#define SBDD_MIB_SECTORS       (1 << (20 - SBDD_SECTOR_SHIFT))
-#define SBDD_NAME              "sbdd"
+#define SBDD_SECTOR_SHIFT       9
+#define SBDD_SECTOR_SIZE        (1 << SBDD_SECTOR_SHIFT)
+#define SBDD_SIZE               PAGE_SIZE
+#define SBDD_NAME               "sbdd"
+#define SBDD_DEVS               4
 
 struct sbdd {
+	struct mutex            datamutex;
 	wait_queue_head_t       exitwait;
-	spinlock_t              datalock;
 	atomic_t                deleting;
 	atomic_t                refs_cnt;
 	sector_t                capacity;
-	u8                      *data;
+	char                    *data[SBDD_DEVS];
 	struct gendisk          *gd;
 	struct request_queue    *q;
+	struct workqueue_struct *wq;
 #ifdef BLK_MQ_MODE
 	struct blk_mq_tag_set   *tag_set;
 #endif
 };
 
+struct sbdd_work {
+#ifdef BLK_MQ_MODE
+	struct request          *rq;
+#else
+	struct bio              *bio;
+#endif
+	struct work_struct      ws;
+};
+
 static struct sbdd      __sbdd;
 static int              __sbdd_major = 0;
-static unsigned long    __sbdd_capacity_mib = 100;
 
 static sector_t sbdd_xfer(struct bio_vec* bvec, sector_t pos, int dir)
 {
-	void *buff = page_address(bvec->bv_page) + bvec->bv_offset;
+	char *buff = page_address(bvec->bv_page) + bvec->bv_offset;
 	sector_t len = bvec->bv_len >> SBDD_SECTOR_SHIFT;
 	size_t offset;
 	size_t nbytes;
@@ -59,14 +71,28 @@ static sector_t sbdd_xfer(struct bio_vec* bvec, sector_t pos, int dir)
 	offset = pos << SBDD_SECTOR_SHIFT;
 	nbytes = len << SBDD_SECTOR_SHIFT;
 
-	spin_lock(&__sbdd.datalock);
+	if (dir) {
+		int i;
 
-	if (dir)
-		memcpy(__sbdd.data + offset, buff, nbytes);
-	else
-		memcpy(buff, __sbdd.data + offset, nbytes);
+		/* Reverse xor with the 1st block */
+		for (i = 0; i < nbytes; i++)
+			__sbdd.data[SBDD_DEVS - 1][offset + i] ^= __sbdd.data[0][offset + i];
 
-	spin_unlock(&__sbdd.datalock);
+		/* Copy new data to the 1st block */
+		memcpy(__sbdd.data[0] + offset, buff, nbytes);
+
+		/* The key factor for the problem to reproduce */
+		// usleep_range(10, 20);
+
+		/* The more time we sleep - the more chance for the problem to appear */
+		msleep(1);
+
+		/* Redo xor with new data */
+		for (i = 0; i < nbytes; i++)
+			__sbdd.data[SBDD_DEVS - 1][offset + i] ^= buff[i];
+	} else {
+		memcpy(buff, __sbdd.data[0] + offset, nbytes);
+	}
 
 	pr_debug("pos=%6llu len=%4llu %s\n", pos, len, dir ? "written" : "read");
 
@@ -75,31 +101,56 @@ static sector_t sbdd_xfer(struct bio_vec* bvec, sector_t pos, int dir)
 
 #ifdef BLK_MQ_MODE
 
-static void sbdd_xfer_rq(struct request *rq)
+static void sbdd_submit_fn(struct work_struct *ws)
 {
+	struct sbdd_work *work = container_of(ws, struct sbdd_work, ws);
+	struct request *rq = work->rq;
 	struct req_iterator iter;
 	struct bio_vec bvec;
 	int dir = rq_data_dir(rq);
 	sector_t pos = blk_rq_pos(rq);
 
+	mutex_lock(&__sbdd.datamutex);
+
 	rq_for_each_segment(bvec, rq, iter)
 		pos += sbdd_xfer(&bvec, pos, dir);
+
+	mutex_unlock(&__sbdd.datamutex);
+
+	kfree(work);
+
+	blk_mq_end_request(rq, BLK_STS_OK);
+	if (atomic_dec_and_test(&__sbdd.refs_cnt))
+		wake_up(&__sbdd.exitwait);
 }
 
 static blk_status_t sbdd_queue_rq(struct blk_mq_hw_ctx *hctx,
                                   struct blk_mq_queue_data const *bd)
 {
-	if (atomic_read(&__sbdd.deleting))
-		return BLK_STS_IOERR;
+	struct sbdd_work *work;
 
-	atomic_inc(&__sbdd.refs_cnt);
+	if (atomic_read(&__sbdd.deleting)) {
+		pr_err("unable to process bio while deleting\n");
+		return BLK_STS_IOERR;
+	}
+
+	if (!atomic_inc_not_zero(&__sbdd.refs_cnt)) {
+		pr_err("unable to get device ref\n");
+		return BLK_STS_IOERR;
+	}
+
+	work = kzalloc(sizeof(struct sbdd_work), GFP_ATOMIC);
+	if (!work) {
+		pr_err("kzalloc() failed for work\n");
+		if (atomic_dec_and_test(&__sbdd.refs_cnt))
+			wake_up(&__sbdd.exitwait);
+		return BLK_STS_IOERR;
+	}
 
 	blk_mq_start_request(bd->rq);
-	sbdd_xfer_rq(bd->rq);
-	blk_mq_end_request(bd->rq, BLK_STS_OK);
-
-	if (atomic_dec_and_test(&__sbdd.refs_cnt))
-		wake_up(&__sbdd.exitwait);
+	work->rq = bd->rq;
+	INIT_WORK(&work->ws, sbdd_submit_fn);
+	queue_work(__sbdd.wq, &work->ws);
 
 	return BLK_STS_OK;
 }
@@ -119,32 +170,57 @@ static struct blk_mq_ops const __sbdd_blk_mq_ops = {
 
 #else
 
-static void sbdd_xfer_bio(struct bio *bio)
+static void sbdd_submit_fn(struct work_struct *ws)
 {
+	struct sbdd_work *work = container_of(ws, struct sbdd_work, ws);
+	struct bio *bio = work->bio;
 	struct bvec_iter iter;
 	struct bio_vec bvec;
 	int dir = bio_data_dir(bio);
 	sector_t pos = bio->bi_iter.bi_sector;
 
+	mutex_lock(&__sbdd.datamutex);
+
 	bio_for_each_segment(bvec, bio, iter)
 		pos += sbdd_xfer(&bvec, pos, dir);
+
+	mutex_unlock(&__sbdd.datamutex);
+
+	bio_endio(bio);
+	kfree(work);
+
+	if (atomic_dec_and_test(&__sbdd.refs_cnt))
+		wake_up(&__sbdd.exitwait);
 }
 
 static blk_qc_t sbdd_make_request(struct request_queue *q, struct bio *bio)
 {
+	struct sbdd_work *work;
+
 	if (atomic_read(&__sbdd.deleting)) {
 		pr_err("unable to process bio while deleting\n");
 		bio_io_error(bio);
 		return BLK_STS_IOERR;
 	}
 
-	atomic_inc(&__sbdd.refs_cnt);
+	if (!atomic_inc_not_zero(&__sbdd.refs_cnt)) {
+		pr_err("unable to get device ref\n");
+		bio_io_error(bio);
+		return BLK_STS_IOERR;
+	}
 
-	sbdd_xfer_bio(bio);
-	bio_endio(bio);
+	work = kzalloc(sizeof(struct sbdd_work), GFP_ATOMIC);
+	if (!work) {
+		pr_err("kzalloc() failed for work\n");
+		bio_io_error(bio);
+		if (atomic_dec_and_test(&__sbdd.refs_cnt))
+			wake_up(&__sbdd.exitwait);
+		return BLK_STS_IOERR;
+	}
 
-	if (atomic_dec_and_test(&__sbdd.refs_cnt))
-		wake_up(&__sbdd.exitwait);
+	work->bio = bio;
+	INIT_WORK(&work->ws, sbdd_submit_fn);
+	queue_work(__sbdd.wq, &work->ws);
 
 	return BLK_STS_OK;
 }
@@ -162,6 +238,7 @@ static struct block_device_operations const __sbdd_bdev_ops = {
 static int sbdd_create(void)
 {
 	int ret = 0;
+	int i, j;
 
 	/*
 	This call is somewhat redundant, but used anyways by tradition.
@@ -175,17 +252,37 @@ static int sbdd_create(void)
 	}
 
 	memset(&__sbdd, 0, sizeof(struct sbdd));
-	__sbdd.capacity = (sector_t)__sbdd_capacity_mib * SBDD_MIB_SECTORS;
+	__sbdd.capacity = (sector_t)SBDD_SIZE >> SBDD_SECTOR_SHIFT;
 
 	pr_info("allocating data\n");
-	__sbdd.data = vzalloc(__sbdd.capacity << SBDD_SECTOR_SHIFT);
-	if (!__sbdd.data) {
-		pr_err("unable to alloc data\n");
+	for (i = 0; i < SBDD_DEVS; i++) {
+		__sbdd.data[i] = (char*)get_zeroed_page(GFP_KERNEL);
+		if (!__sbdd.data[i]) {
+			pr_err("get_zeroed_page() failed\n");
+			return -ENOMEM;
+		}
+	}
+
+	pr_info("initializing data\n");
+	for (i = 0; i < SBDD_DEVS - 1; i++)
+		get_random_bytes(__sbdd.data[i], SBDD_SIZE);
+
+	for (i = 0; i < SBDD_SIZE; i++) {
+		__sbdd.data[SBDD_DEVS - 1][i] = __sbdd.data[0][i];
+		for (j = 1; j < SBDD_DEVS - 1; j++)
+			__sbdd.data[SBDD_DEVS - 1][i] ^= __sbdd.data[j][i];
+	}
+
+	pr_info("allocating workqueue\n");
+	__sbdd.wq = alloc_workqueue(SBDD_NAME "_wq", WQ_UNBOUND, 0);
+	if (!__sbdd.wq) {
+		pr_err("alloc_workqueue() failed\n");
 		return -ENOMEM;
 	}
 
-	spin_lock_init(&__sbdd.datalock);
+	mutex_init(&__sbdd.datamutex);
 	init_waitqueue_head(&__sbdd.exitwait);
+	atomic_set(&__sbdd.refs_cnt, 1);
 
 #ifdef BLK_MQ_MODE
 	pr_info("allocating tag_set\n");
@@ -256,9 +353,17 @@ static int sbdd_create(void)
 
 static void sbdd_delete(void)
 {
-	atomic_set(&__sbdd.deleting, 1);
+	int i;
 
+	atomic_set(&__sbdd.deleting, 1);
+	atomic_dec(&__sbdd.refs_cnt);
 	wait_event(__sbdd.exitwait, !atomic_read(&__sbdd.refs_cnt));
+
+	if (__sbdd.wq) {
+		pr_info("destroying workqueue\n");
+		flush_workqueue(__sbdd.wq);
+		destroy_workqueue(__sbdd.wq);
+	}
 
 	/* gd will be removed only after the last reference put */
 	if (__sbdd.gd) {
@@ -284,9 +389,10 @@ static void sbdd_delete(void)
 		kfree(__sbdd.tag_set);
 #endif
 
-	if (__sbdd.data) {
-		pr_info("freeing data\n");
-		vfree(__sbdd.data);
+	pr_info("freeing data\n");
+	for (i = 0; i < SBDD_DEVS; i++) {
+		if (__sbdd.data[i])
+			free_page((unsigned long)__sbdd.data[i]);
 	}
 
 	memset(&__sbdd, 0, sizeof(struct sbdd));
@@ -307,14 +413,14 @@ static int __init sbdd_init(void)
 {
 	int ret = 0;
 
-	pr_info("starting initialization...\n");
+	pr_info("starting...\n");
 	ret = sbdd_create();
 
 	if (ret) {
-		pr_warn("initialization failed\n");
+		pr_warn("starting failed\n");
 		sbdd_delete();
 	} else {
-		pr_info("initialization complete\n");
+		pr_info("starting complete\n");
 	}
 
 	return ret;
@@ -332,14 +438,64 @@ static void __exit sbdd_exit(void)
 	pr_info("exiting complete\n");
 }
 
+static int sbdd_consistency_check(char *buf, struct kernel_param const *kp)
+{
+	int written;
+	int i, j;
+
+	pr_info("performing check\n");
+	written = snprintf(buf, PAGE_SIZE, "ok\n");
+	mutex_lock(&__sbdd.datamutex);
+
+	for (i = 0; i < SBDD_SIZE; i++) {
+		char byte = __sbdd.data[0][i];
+
+		for (j = 1; j < SBDD_DEVS - 1; j++)
+			byte ^= __sbdd.data[j][i];
+
+		if (byte != __sbdd.data[SBDD_DEVS - 1][i]) {
+			written = snprintf(buf, PAGE_SIZE, "byte %d check failed\n", i);
+			break;
+		}
+	}
+
+	mutex_unlock(&__sbdd.datamutex);
+	return written;
+}
+
+static int sbdd_consistency_reset(char const *buf, struct kernel_param const *kp)
+{
+	int i, j;
+
+	pr_info("resetting data\n");
+	mutex_lock(&__sbdd.datamutex);
+
+	for (i = 0; i < SBDD_DEVS - 1; i++)
+		get_random_bytes(__sbdd.data[i], SBDD_SIZE);
+
+	for (i = 0; i < SBDD_SIZE; i++) {
+		__sbdd.data[SBDD_DEVS - 1][i] = __sbdd.data[0][i];
+		for (j = 1; j < SBDD_DEVS - 1; j++)
+			__sbdd.data[SBDD_DEVS - 1][i] ^= __sbdd.data[j][i];
+	}
+
+	mutex_unlock(&__sbdd.datamutex);
+	return 0;
+}
+
+static struct kernel_param_ops const sbdd_consistency_ops = {
+	.set = sbdd_consistency_reset,
+	.get = sbdd_consistency_check,
+};
+
 /* Called on module loading. Is mandatory. */
 module_init(sbdd_init);
 
 /* Called on module unloading. Unloading module is not allowed without it. */
 module_exit(sbdd_exit);
 
-/* Set desired capacity with insmod */
-module_param_named(capacity_mib, __sbdd_capacity_mib, ulong, S_IRUGO);
+/* Operations to check/reset consistency of the "stripe" */
+module_param_cb(consistency, &sbdd_consistency_ops, NULL, S_IRUGO | S_IWUSR);
 
 /* Note for the kernel: a free license module. A warning will be outputted without it. */
 MODULE_LICENSE("GPL");
